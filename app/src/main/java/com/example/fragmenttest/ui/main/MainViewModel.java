7252f3ac4e6db42c6ae184625e809855325fb32e
takasismomo <takasi_s@momo.so-net.ne.jp>
2021-05-13 15:38:13 +0900

    初期状態

---
+++ app/src/main/java/com/example/fragmenttest/ui/main/MainViewModel.java
@@ -0,0 +1,8 @@
+package com.example.fragmenttest.ui.main;
+
+import androidx.lifecycle.ViewModel;
+
+public class MainViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+    boolean flagButton;
+}
