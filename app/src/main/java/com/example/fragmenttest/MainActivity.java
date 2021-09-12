58f4771a634e1128f8dd94bc3bad8e10882f1e39
takasismomo <takasi_s@momo.so-net.ne.jp>
2021-09-12 18:18:07 +0900

    初期状態

--- app/src/main/java/com/example/fragmenttest/MainActivity.java
+++ app/src/main/java/com/example/fragmenttest/MainActivity.java
@@ -1,23 +1,33 @@
 package com.example.fragmenttest;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Binder;
 import android.os.Bundle;
 
 import com.example.fragmenttest.ui.main.MainFragment;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends FragmentActivity {
+    private static final int NUM_PAGES=5;
+    private ViewPager2 viewPager;
+    private FragmentStateAdapter pagerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.main_activity);
-        if (savedInstanceState == null) {
+        viewPager = findViewById(R.id.pager);
+        pagerAdapter = new ScreenSlidePagerAdapter(this);
+        viewPager.setAdapter(pagerAdapter);
+/*        if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
                     .replace(R.id.container, MainFragment.newInstance())
                     .commitNow();
-        }
+        } */
     }
 }
